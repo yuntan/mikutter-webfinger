338b79104ddb4304c4dd8f3635a549bc33aaa05a
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-08 00:03:18 +0900

    Objectその他のModelにBasisModelとIdentityをinclude

--- model_ext.rb
+++ model_ext.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 require_relative 'fetch'
-require_relative 'identity'
 require_relative 'modelbuilder'
+require_relative 'mixin/basismodel'
+require_relative 'mixin/identity'
 require_relative 'model/activity'
 require_relative 'model/actor'
 require_relative 'model/collection'
 require_relative 'model/object'
 
 module Plugin::WebFinger
-  Base.send :prepend, Identity
-
   class Activity
+    include BasisModel
+    include Identity
+
     def actor
       @actor ||= (Actor.find actor_uri or PW.fetch actor_uri)
     end
@@ -23,6 +25,9 @@ module Plugin::WebFinger
   end
 
   class Actor
+    include BasisModel
+    include Identity
+
     def outbox
       @outbox ||= (Collection.find outbox_uri or PW.fetch outbox_uri)
     end
@@ -37,6 +42,9 @@ module Plugin::WebFinger
   end
 
   class Collection
+    include BasisModel
+    include Identity
+
     attr_reader :items
 
     def page_next_uri
@@ -67,6 +75,9 @@ module Plugin::WebFinger
   end
 
   class Object
+    include BasisModel
+    include Identity
+
     def attributed_to
       (uri = attributed_to_uri) or return nil
       @attributed_to ||= (Actor.find uri or PW.fetch uri)
