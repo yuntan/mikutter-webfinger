89e78a5eef162db4efde9d3afdb31d6cb5cc7fc3
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-07 23:46:42 +0900

    Identity で ModelExtend#store_datum を使うよう修正
    storage に WeakStorage を使うよう修正

---
+++ mixin/identity.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Plugin::WebFinger
+  module Identity
+    module ClassMethods
+      # override Diva::ModelExtend#find_by_uri
+      def find_by_uri(uri)
+        uri = (uri.is_a? Diva::URI and uri or Diva::URI.new uri)
+        obj = storage[uri.hash]
+        obj.is_a? self or return nil
+        notice "using cached #{obj}"
+        obj
+      end
+
+      # override Diva::ModelExtend#store_datum
+      def store_datum(model)
+        notice "model: #{model}"
+
+        type_strict model => self
+        model.uri or return
+        uri = model.uri
+        uri = (uri.is_a? Diva::URI and uri or Diva::URI.new uri)
+        storage[uri.hash] = model
+      end
+
+    private
+
+      def storage
+        @storage ||=
+          WeakStorage.new Integer, Diva::Model, name: 'WebFinger Objects'
+      end
+    end
+
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+  end
+end
