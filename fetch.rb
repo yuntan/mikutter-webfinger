984b37fef188ea42f84239ff3147a8785dcd32ac
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-07 23:59:09 +0900

    fetch(uri)をDeferred化，acct_from_uri(uri)を追加

--- fetch.rb
+++ fetch.rb
@@ -6,45 +6,50 @@ require 'rexml/document'
 require_relative 'modelbuilder'
 
 module Plugin::WebFinger
+  RE_ACCT = /^@?(?<name>[a-z0-9\-_]+)@(?<domain>[a-z0-9\-]+(\.[a-z0-9\-]+)+)$/.freeze
+
+module_function
+
   def fetch(uri)
-    notice "fetch(#{uri})"
+    Deferred.new do
+      uri or raise ArgumentError.new 'uri is nil'
 
-    uri or return nil
+      notice "fetch uri: #{uri}"
 
-    data =
-      begin
-        # Activity Streams 2.0 object
-        # https://www.w3.org/TR/activitystreams-vocabulary/
-        JSON.parse uri.read 'Accept' => 'application/activity+json'
-      rescue JSON::ParserError
-        return nil
-      end
+      data =
+        begin
+          # Activity Streams 2.0 object
+          # https://www.w3.org/TR/activitystreams-vocabulary/
+          JSON.parse uri.read 'Accept' => 'application/activity+json'
+        rescue JSON::ParserError
+          error $!
+          raise $!
+        end
 
-    ModelBuilder.new(data).build
+      ModelBuilder.new(data).build
+    end
   end
 
-  def discover(query)
-    notice "discover(#{query})"
+  def uri_from_acct(acct)
+    Deferred.new do
+      notice "uri_from_acct acct: #{acct}"
 
-    (m = RE_ACCT.match query || RE_PROFILE_URI.match(query)) or return nil
+      (m = RE_ACCT.match acct) \
+        or raise ArgumentError.new "invalid acct: #{acct}"
+      acct = "#{m[:name]}@#{m[:domain]}"
 
-    acct = "#{m[:name]}@#{m[:domain]}"
+      # RFC6415 Web Host Metadata
+      uri = URI.parse "https://#{m[:domain]}/.well-known/host-meta"
+      doc = REXML::Document.new uri.read 'Accept' => 'application/xrd+xml'
+      template = REXML::XPath.match(doc, '/XRD/Link[@rel="lrdd"]') \
+        .first.attribute('template').to_s
 
-    # RFC6415 Web Host Metadata
-    uri = URI.parse "https://#{m[:domain]}/.well-known/host-meta"
-    doc = REXML::Document.new uri.read 'Accept' => 'application/xrd+xml'
-    template = REXML::XPath.match(doc, '/XRD/Link[@rel="lrdd"]') \
-      .first.attribute('template').to_s
+      # RFC7033 WebFinger
+      uri = URI.parse template.sub(/\{uri\}/, "acct:#{acct}")
+      data = JSON.parse uri.read 'Accept' => 'application/jrd+json'
+      link = data['links'].find { |l| l['rel'] == 'self' }
 
-    # RFC7033 WebFinger
-    uri = URI.parse template.sub(/\{uri\}/, "acct:#{acct}")
-    data = JSON.parse uri.read 'Accept' => 'application/jrd+json'
-    link = data['links'].find { |l| l['rel'] == 'self' }
-
-    uri = URI.parse link['href']
-
-    fetch(uri).tap { |actor| actor.acct = acct }
+      URI.parse link['href']
+    end
   end
-
-  module_function :fetch, :discover
 end
